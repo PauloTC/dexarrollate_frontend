7c29624f1a4bf260f3c501e7f5d9773f2c2c3e57
iLTW1n <jose.osk01@gmail.com>
2024-06-05 10:04:16 -0500

    feat(g): login mobile and desktop

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 
 const LoginComponent = () => {
   const router = useRouter();
-
   const [documentType, setDocumentType] = useState("dni");
 
   const handleSubmit = (event: any) => {
@@ -20,19 +19,39 @@ const LoginComponent = () => {
   };
 
   return (
-    <>
-      <div>
-        <div className="flex sm:dl-hidden">
+    <div className='dl-flex dl-flex-col lg:dl-grid lg:dl-grid-cols-6 dl-h-screen'>
+      <div className="dl-relative lg:dl-col-span-4 dl-h-40 lg:dl-h-screen">
+        <div className='dl-flex lg:dl-hidden'>
           <Image
-            priority
             alt="banner"
-            layout="responsive"
-            width={1200}
-            height={800}
-            src="/login/banner_mobile.png"
+            fill
+            objectFit='cover'
+            sizes='100vw'
+            src="/login/banner-mobile.png"
           />
         </div>
-        <div className="px-4 dl-py-8">
+        <div className='dl-hidden lg:dl-block'>
+          <Image
+            alt="banner"
+            fill
+            sizes='100vw'
+            src="/login/banner-desktop.jpg"
+          />
+        </div>
+      </div>
+      <div
+        className="
+          dl-py-8
+          dl-px-4
+          lg:dl-col-span-2
+          lg:dl-px-20
+          lg:dl-flex
+          dl-flex-col
+          dl-items-center
+          dl-justify-around
+        "
+      >
+        <div className='dl-w-full dl-max-w-xl'>
           <Image
             className="mb-6"
             alt="logo"
@@ -48,6 +67,7 @@ const LoginComponent = () => {
             <DlSelect
               onChange={handleSelectChange}
               value="dni"
+              size='lg'
               items={[
                 { value: "dni", label: "DNI" },
                 { value: "ce", label: "CE" },
@@ -57,9 +77,10 @@ const LoginComponent = () => {
               maxLength={documentType === "dni" ? 8 : 7}
               required
               placeholder={`Número de ${documentType.toUpperCase()}`}
+              size='lg'
             />
-            <DlInput required type="password" placeholder="Contraseña" />
-            <DlButton type="submit" className="dl-mb-6" block={true}>
+            <DlInput required type="password" placeholder="Contraseña" size='lg' />
+            <DlButton type="submit" className="dl-mb-6" block size='lg'>
               Ingresar
             </DlButton>
           </form>
@@ -69,7 +90,7 @@ const LoginComponent = () => {
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
