560afd13348f7f3c2898fb07ee87b35db5a3b8d4
iLTW1n <jose.osk01@gmail.com>
2024-06-06 17:03:17 -0500

    feat(g): done home page tabs videos and sidebar

--- src/app/inicio/layout.tsx
+++ src/app/inicio/layout.tsx
@@ -1,12 +1,30 @@
 "use client";
 
 import HeaderComponent from "@/components/header";
+import { DlSidebar } from '@alicorpdigital/dali-react';
 
 const Layout = ({ children }: any) => {
   return (
     <>
       <HeaderComponent />
-      {children}
+      <div className='dl-grid lg:dl-grid-cols-12 xl:dl-gap-12'>
+        <div className='dl-hidden lg:dl-grid lg:dl-col-span-3'>
+        <DlSidebar
+          open
+          label='Supervisor'
+          titleLabel='Paulo Tejada'
+          items={[
+            {
+              key: 'resources',
+              label: 'Recursos'
+            }
+          ]}
+        />
+        </div>
+        <div className='dl-col-span-10 lg:dl-col-span-9'>
+          {children}
+        </div>
+      </div>
     </>
   );
 };
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -8,27 +8,50 @@ import { Home } from "@/api";
 const homeCtrl = new Home();
 
 interface Video {
-  data: {
-    attributes: {
-      url: string;
-      name: string;
-    };
-    id: string;
-  }[];
+  attributes: {
+    url: string;
+    name: string;
+  };
+  id: string;
 }
 
 const HomePage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [videos, setVideos] = useState<Video[]>([]);
 
   useEffect(() => {
     (async () => {
-      const { data } = await homeCtrl.getHomeData();
-
-      setVideos(data?.attributes?.video);
-
-      console.log("data", data.attributes.video);
+      setLoading(true);
+      const data = await homeCtrl.getHomeData();
+      setVideos(data);
+      setLoading(false);
     })();
   }, []);
+
+  const formattedVideos = () => {
+    const tabs = [
+      {
+        key: "calendario_semanal.mp4",
+        title: "Calendario semanal",
+      },
+      {
+        key: "rutina_comercial.mp4",
+        title: "Rutina comercial ",
+      },
+      {
+        key: "playbook_comercial.mp4",
+        title: "Playbook",
+      }
+    ]
+
+    return tabs.map((tab => {
+      const video = videos.find(item => item.attributes.name === tab.key);
+      return {
+        ...tab,
+        url: video?.attributes?.url
+      }
+    }))
+  }
 
   return (
     <>
@@ -96,49 +119,28 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <div className="dl-p-4 dl-container dl-mx-auto lg:dl-p-0 lg:dl-py-8">
-        <DlTabs
-          items={[
-            {
-              children: (
-                <div className="dl-flex dl-justify-center">
-                  {/* <video width="790" height="440" controls>
-                    <source
-                      src="http://localhost:1337/uploads/calendario_semanal_e633475c67.mp4"
-                      type="video/mp4"
-                    />
-                  </video> */}
-                </div>
-              ),
-              key: "calendario-semanal",
-              title: "Calendario semanal",
-            },
-            {
-              children: (
-                <div className="dl-flex dl-justify-center">
-                  {/* <video width="790" height="440" controls>
-                    <source
-                      src="http://localhost:1337/uploads/calendario_semanal_e633475c67.mp4"
-                      type="video/mp4"
-                    />
-                  </video> */}
-                </div>
-              ),
-              key: "rutina-comercial",
-              title: "Rutina comercial ",
-            },
-            {
-              children: "Tab 3 content",
-              key: "playbook",
-              title: "Playbook",
-            },
-          ]}
-        />
-      </div>
-      {videos?.data?.map((video, index) => (
-        <p key={index}>{video.attributes.name}</p>
-      ))}
-
+      {!loading &&
+        <div className="dl-p-4 dl-container dl-mx-auto lg:dl-p-0 lg:dl-py-8">
+          <DlTabs
+            items={formattedVideos().map(video => {
+              return {
+                key: video.key,
+                title: video.title,
+                children: (
+                  <div className="dl-flex dl-justify-center">
+                    <video width="790" height="440" controls key={video.key}>
+                      <source
+                        src={video.url}
+                        type="video/mp4"
+                      />
+                    </video>
+                  </div>
+                )
+              }
+            })}
+          />
+        </div>
+      }
       <Soon />
     </>
   );
