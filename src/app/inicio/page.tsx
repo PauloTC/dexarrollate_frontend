b9d675053539e95246c77f65d2bd447b8e5ded8f
Paulo Tejada <paulotejada.123a@gmail.com>
2024-06-06 18:07:26 -0500

    conditional sidebar

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -1,16 +1,6 @@
 "use client";
-import {
-  DlButton,
-  DlIcon,
-  DlTag,
-  DlIconNamesProp,
-} from "@alicorpdigital/dali-react";
-import Image from "next/image";
-import { useAuth } from "@/hooks/useAuth";
-import HomePage from '@/screens/home';
+import HomePage from "@/screens/home";
 
 export default function Home() {
-  const { user } = useAuth();
-
-  return <HomePage />
+  return <HomePage />;
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,19 +4,17 @@ import Image from "next/image";
 
 const HeaderComponent = () => {
   return (
-    <>
-      <div
-        style={{ borderBottom: "1px solid #6c6c6c" }}
-        className="dl-p-4 dl-flex dl-items-center dl-justify-between"
-      >
-        <Image alt="logo" width={168} height={20} src="/login/logo.svg" />
-        <DlIcon
-          className="dl-text-al-primary-medium"
-          size="lg"
-          name="hamburger"
-        />
-      </div>
-    </>
+    <div
+      style={{ borderBottom: "1px solid #6c6c6c" }}
+      className="dl-p-4 dl-flex dl-items-center dl-justify-between"
+    >
+      <Image alt="logo" width={168} height={20} src="/login/logo.svg" />
+      <DlIcon
+        className="dl-text-al-primary-medium"
+        size="lg"
+        name="hamburger"
+      />
+    </div>
   );
 };
 
