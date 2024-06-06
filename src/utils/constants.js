122257ecd13a3bc40d33a539ff2cf99b98e5f0b7
Paulo Tejada <paulotejada.123a@gmail.com>
2024-06-06 13:28:12 -0500

    adding backend qa enviroment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: "http://localhost:1337/",
-  API_URL: "http://localhost:1337/api",
+  SERVER_HOST: "https://dexarrollate-qa.up.railway.app/",
+  API_URL: "https://dexarrollate-qa.up.railway.app/api",
   ENDPOINTS: {
     AUTH: {
       LOGIN: "/auth/local",
